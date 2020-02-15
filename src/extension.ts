4f4562839ecf1fa8e2b0af5b1fce33928b626484
dozsolti <d.zsolti@yahoo.com>
2020-02-15 20:48:46 +0200

    Update after review
    Comments remains saved
    The target was to change only the properties order, noting else

--- src/extension.ts
+++ src/extension.ts
@@ -16,32 +16,43 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.sortcss', () => {
-		// The code you place here will be executed every time your command is executed
+		try {
 
-		var editor = vscode.window.activeTextEditor;
 
-		if (!editor) {
-			return vscode.window.showErrorMessage("no Open text editor");
+			// The code you place here will be executed every time your command is executed
+
+			var editor = vscode.window.activeTextEditor;
+
+			if (!editor) {
+				return vscode.window.showErrorMessage("no Open text editor");
+			}
+
+			let originalFileContent = editor.document.getText();
+			let css = CSSJSON.toJSON(originalFileContent, {
+				ordered: true,
+				comments: true
+			});
+
+			css = formatter(css);
+			Order(css, 'importante');
+
+			let newCss = CSSJSON.toCSS(css);
+			var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+
+			let allDocumentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, lastLine.range.end.character + 1);
+
+			editor.edit(editBuilder => {
+				editBuilder.replace(allDocumentRange, newCss);
+			});
+
+
+
+			return vscode.window.showInformationMessage("Reorder successfully");
+		} catch (err) {
+			console.log(err);
+			return vscode.window.showErrorMessage(err);
+
 		}
-
-		let originalFileContent = editor.document.getText();
-		let css = CSSJSON.toJSON(originalFileContent);
-
-		Order(css, 'importante');
-
-		let newCss = CSSJSON.toCSS(css);
-
-		var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-
-		let allDocumentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, lastLine.range.end.character + 1);
-
-		editor.edit(editBuilder => {
-			editBuilder.replace(allDocumentRange, newCss);
-		});
-
-
-
-		return vscode.window.showInformationMessage("Reorder successfully");
 	});
 
 	let disposableAlphabetical = vscode.commands.registerCommand('extension.sortcssalphabetical', () => {
@@ -54,8 +65,12 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		let originalFileContent = editor.document.getText();
-		let css = CSSJSON.toJSON(originalFileContent);
+		let css = CSSJSON.toJSON(originalFileContent, {
+			ordered: true,
+			comments: true
+		});
 
+		css = formatter(css);
 		Order(css, 'alphabetical');
 
 		let newCss = CSSJSON.toCSS(css);
@@ -74,16 +89,42 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 }
+function formatter(start:any){
+	let end :any = { attributes: {}, children: {}};
 
-function Order(css: any, orderType:string){
-	if(orderType == "importante"){
+	for(let k in start){
+		let val = start[k];
+		if((typeof val)=='string'){
+			let keyOfAComment = 'this is a Zascal Key '+Object.keys(end.children).length+1;
+			end.children[keyOfAComment] = val;
+		}else{
+			if((typeof val.value)=='string'){
+			end	.children[val.name] = val.value;
+			}
+			else{
+				let attrs = val.value;
+				for(let attrKey in attrs){
+					if(!(val.name in end.children))
+					end.children[val.name] = { attributes: {}, children: {}};
+					if(attrs[attrKey].name)
+					end.children[val.name].attributes[attrs[attrKey].name] = attrs[attrKey].value;
+				}
+			}
+
+		}
+	}
+	return end;
+}
+function Order(css: any, orderType: string) {
+	if (orderType == "importante") {
 		css['attributes'] = sortAttributesImportante(css['attributes']);
 	}
-	else if(orderType == "alphabetical"){
+	else if (orderType == "alphabetical") {
 		css['attributes'] = sortAttributesAlfabeticly(css['attributes']);
 	}
-	for(let childKey of Object.keys(css['children'])){
-		Order(css['children'][childKey],orderType);
+	for (let childKey of Object.keys(css['children'])) {
+		if((typeof css['children'][childKey])!='string')
+			Order(css['children'][childKey], orderType); 
 	}
 }
 function sortAttributesAlfabeticly(attributes: any) {
@@ -125,4 +166,4 @@ function sortAttributesImportante(attributes: any) {
 	return newOrder;
 }
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
